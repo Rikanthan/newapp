84e117fd3277bae2494051af34039a0d995ca303
ricky <rikanthanricky@gmail.com>
2020-02-03 08:05:12 +0530

    array

--- app/src/main/java/com/example/cs/array_a.java
+++ app/src/main/java/com/example/cs/array_a.java
@@ -17,17 +17,9 @@ public class array_a extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_array_a);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
+
     }
 
 
